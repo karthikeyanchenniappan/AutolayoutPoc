8c9f50644061626874c3c8259505854c0477b9e4
KarthiKeyan <karthikeyan.chenniappan@hubino.com>
2015-04-07 13:45:00 +0530

    update

---
+++ rotation_samlpe_poc/rotation_samlpe_poc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rotation_samlpe_poc
+//
+//  Created by Hubino-Mac on 12/02/15.
+//  Copyright (c) 2015 Hubino-Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
